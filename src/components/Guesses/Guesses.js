bb4e4ca1a34531781d788b95e34f2c3240e45532
Andres Ferrada <an.ferrada@gmail.com>
2024-01-10 14:40:22 -0300

    Guesses component added

---
+++ src/components/Guesses/Guesses.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Guesses() {
+  return (
+    <div>
+      <div class="guess-results">
+        <p class="guess">FIRST</p>
+        <p class="guess">GUESS</p>
+      </div>
+    </div>
+  )
+}
+
+export default Guesses
