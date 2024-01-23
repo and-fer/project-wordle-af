977307ca729b2a29d9e178f09efa240728c6fb02
Andres Ferrada <an.ferrada@gmail.com>
2024-01-23 12:51:40 -0300

    Exercise 3 My solution

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import { range } from '../../utils'
+import { checkGuess } from '../../game-helpers'
 
 function Guess({ guess }) {
+  function classNameStatus(num) {
+    let userLetter = checkGuess(guess, 'WALLO')
+    const baseClass = 'cell'
+    let statusClass = ''
+
+    if (userLetter === null) {
+      return baseClass
+    }
+
+    if (userLetter[num].status === 'correct') {
+      statusClass = 'correct'
+    }
+    if (userLetter[num].status === 'misplaced') {
+      statusClass = 'misplaced'
+    }
+    if (userLetter[num].status === 'incorrect') {
+      statusClass = 'incorrect'
+    }
+
+    return `${baseClass} ${statusClass}`
+  }
+
   return (
     <div>
       <p className="guess">
-        {range(5).map(numCol => (
-          <span key={numCol} className="cell">
-            {guess ? guess[numCol] : undefined}
-          </span>
-        ))}
+        {range(5).map(numCol => {
+          return (
+            <span key={numCol} className={classNameStatus(numCol)}>
+              {guess ? guess[numCol] : undefined}
+            </span>
+          )
+        })}
       </p>
     </div>
   )
