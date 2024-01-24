88a4e39f842969664e21342582df112e7605c634
Andres Ferrada <an.ferrada@gmail.com>
2024-01-23 22:49:48 -0300

    Cleaning and rearrange elements to work in the official solution version

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,37 +3,43 @@ import { range } from '../../utils'
 import { checkGuess } from '../../game-helpers'
 
 function Guess({ guess, answer }) {
-  function classNameStatus(num) {
-    let userLetter = checkGuess(guess, 'WALLO')
-    const baseClass = 'cell'
-    let statusClass = ''
+  // function classNameStatus(num) {
+  //   let userLetter = checkGuess(guess, 'WALLO')
+  //   const baseClass = 'cell'
+  //   let statusClass = ''
 
-    if (userLetter === null) {
-      return baseClass
-    }
+  //   if (userLetter === null) {
+  //     return baseClass
+  //   }
 
-    if (userLetter[num].status === 'correct') {
-      statusClass = 'correct'
-    }
-    if (userLetter[num].status === 'misplaced') {
-      statusClass = 'misplaced'
-    }
-    if (userLetter[num].status === 'incorrect') {
-      statusClass = 'incorrect'
-    }
+  //   if (userLetter[num].status === 'correct') {
+  //     statusClass = 'correct'
+  //   }
+  //   if (userLetter[num].status === 'misplaced') {
+  //     statusClass = 'misplaced'
+  //   }
+  //   if (userLetter[num].status === 'incorrect') {
+  //     statusClass = 'incorrect'
+  //   }
 
-    return `${baseClass} ${statusClass}`
+  //   return `${baseClass} ${statusClass}`
+  // }
+
+  function Cell({ letter, status }) {
+    return (
+      <span className={}>
+        {/* {guess ? guess[numCol] : undefined} */}
+      </span>
+    )
   }
+
+  const guessStatus = checkGuess(guess, answer)
 
   return (
     <div>
       <p className="guess">
         {range(5).map(numCol => {
-          return (
-            <span key={numCol} className={classNameStatus(numCol)}>
-              {guess ? guess[numCol] : undefined}
-            </span>
-          )
+          return <Cell key={numCol} />
         })}
       </p>
     </div>
